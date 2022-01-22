981bbdb7ae1f7cbe5ff312c929477fbfc4b3569e
Michal Lange <micha.soczewicy@gmail.com>
2022-01-22 18:48:35 +0100

    Reset score Player data on start of game

--- Assets/Scripts/UI/UIGamePanel.cs
+++ Assets/Scripts/UI/UIGamePanel.cs
@@ -9,6 +9,11 @@ public class UIGamePanel : MonoBehaviour
     [SerializeField]
     private GameObject m_PauseMenuRoot = null;
 
+    public void Start()
+    {
+        PlayerPrefs.SetInt("score", 0);
+    }
+
     public void SetPoints(int points)
     {
         m_PointsText.text = points.ToString();
